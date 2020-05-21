6e5a815a7177fef1d364bc25559164b3d5eca9dc
Timax <timax2@wp.pl>
2020-05-21 15:37:51 +0200

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoggingComponent } from './logging/logging.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'logging', component: LoggingComponent },
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
